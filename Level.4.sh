90cc6edf238b6b33eaca3390f2c1978e25a6f659
tjs6232 <106604088+tjs6232@users.noreply.github.com>
2022-06-13 09:56:10 -0400

    Create Level.4.sh

---
+++ Level.4.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#Make Hash from User ID
+declare USER_ID=""
+echo "Enter you PSU user ID: "
+read USER_ID
+USER_HASH=$(echo -n "$USER_ID" | md5sum)
+echo -n "$USER_HASH" > inhere.txt
+
+mkdir Lion_Shrine Old_Main Westgate Creamery Pollock Pegula Beaver_Stadium Buckeye
+
+cd Buckeye
+
+touch inhere
+
+cd ..
+
+mv inhere.txt /home/tjs6232/Buckeye/inhere
+
+echo "Welcome to level 4.  Use all of the commands that you have used so far to"
+
+echo "Find the Password in the Hidden file"
