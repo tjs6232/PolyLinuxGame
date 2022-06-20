57eaf2b54cd934c6b5d593a85ea11ba675976bcb
tjs6232 <106604088+tjs6232@users.noreply.github.com>
2022-06-20 11:39:19 -0400

    Create Level3.sh

---
+++ Level3.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#trap ' ' 2 20
+
+rc-update delete hwclock boot
+
+rc-service hwclock restart
+
+date +"%Y-%m-%d" > currentDate.txt
+
+currentDate=$(head -n 1 currentDate.txt)
+
+USER_ID=""
+
+echo "Enter your PSU email (xyz1234@psu.edu): "
+
+read USER_ID
+
+echo -n "$USER_ID" > userID.txt
+
+USER_HASH=$(echo -n "$USER_ID$currentDate" | md5sum)
+
+#Delete all the above once Setup1-5 is complete
+
+mkdir Westgate
+
+mv userID.txt home/Westgate
+
+cd Westgate
+
+echo "***************************************"
+echo "*   Welcome to Level 3 of The PolyLinux Game     *"
+echo "*             Good Luck!              *"
+echo "*    You created this session on:     *"
+echo "*             $currentDate            *"
+echo "***************************************"
+echo "*Use the cat command on the file userID to find the password*"
