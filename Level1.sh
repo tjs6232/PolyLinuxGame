8ee569200e9281d7ad1bfe94edc187c555314db0
tjs6232 <106604088+tjs6232@users.noreply.github.com>
2022-06-20 10:47:40 -0400

    Update Level1.sh

--- Level1.sh
+++ Level1.sh
@@ -20,8 +20,15 @@ echo -n "$USER_ID" > userID.txt
 
 USER_HASH=$(echo -n "$USER_ID$currentDate" | md5sum)
 
+#Delete all the above once Setup1-5 is complete
+
 mkdir "$USER_HASH"
 
 
-
-
+echo "***************************************"
+echo "*   Welcome to Level 1 of The PolyLinux Game     *"
+echo "*             Good Luck!              *"
+echo "*    You created this session on:     *"
+echo "*             $currentDate            *"
+echo "***************************************"
+echo "*Use the ls command to find the password*"
