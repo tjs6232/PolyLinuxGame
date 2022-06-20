4781a76266f016026c40c4b15ab90c8fe61d2a2e
tjs6232 <106604088+tjs6232@users.noreply.github.com>
2022-06-20 10:12:01 -0400

    Update Level1.sh

--- Level1.sh
+++ Level1.sh
@@ -1,1 +1,16 @@
 #!/bin/bash
+#trap ' ' 2 20
+rc-update delete hwclock boot
+rc-service hwclock restart
+date +"%Y-%m-%d" > currentDate.txt
+currentDate=$(head -n 1 currentDate.txt)
+USER_ID=""
+echo "Enter your PSU email (xyz1234@psu.edu): "
+read USER_ID
+echo -n "$USER_ID" > userID.txt
+USER_HASH=$(echo -n "$USER_ID$currentDate" | md5sum)
+echo -n "$USER_HASH" > userHash.txt
+
+userName="polylinuxgame"
+newPass="Password1"
+useradd -p $newPass -m $userName
