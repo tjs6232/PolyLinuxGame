d84e959d49972397f85087b3a1a37754b657d5fd
tjs6232 <106604088+tjs6232@users.noreply.github.com>
2022-06-21 13:40:42 -0400

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,50 @@
+mkdir OldMain
+cd OldMain
+touch Bell
+touch Door
+touch Presidents
+touch Office
+cd ..
+
+mkdir BeaverStadium
+cd BeaverStadium
+touch "$USER_HASH"
+mkdir TheHub
+cd TheHub
+touch Fish
+touch Food
+touch Lawn
+touch Rooms
+cd ..
+
+mkdir Arboretum
+cd Arboretum
+touch Flowers
+touch Walking
+touch Birds
+touch Water
+cd ..
+
+mkdir Pegula
+cd Pegula
+touch Hockey
+touch Players
+touch Ice
+touch Arena
+cd ..
+
+mkdir HappyValley
+cd Happy Valley
+touch bars
+touch people
+touch football
+touch students
+cd ..
+
+mkdir WestGate
+cd Westgate
+touch Computers
+touch Cybertorium
+touch Professors
+touch ClassRooms
+cd ..
