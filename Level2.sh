7d9175f8f6e81bb75b9874b10cf50c13a6e6026e
tjs6232 <106604088+tjs6232@users.noreply.github.com>
2022-06-22 09:22:49 -0400

    Update Level2.sh

--- Level2.sh
+++ Level2.sh
@@ -1,26 +1,3 @@
-#!/bin/bash
-
-#trap ' ' 2 20
-
-rc-update delete hwclock boot
-
-rc-service hwclock restart
-
-date +"%Y-%m-%d" > currentDate.txt
-
-currentDate=$(head -n 1 currentDate.txt)
-
-USER_ID=""
-
-echo "Enter your PSU email (xyz1234@psu.edu): "
-
-read USER_ID
-
-echo -n "$USER_ID" > userID.txt
-
-USER_HASH=$(echo -n "$USER_ID$currentDate" | md5sum)
-
-#Delete all the above once Setup1-5 is complete
 
 mkdir BeaverStadium
 
