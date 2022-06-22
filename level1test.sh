04f6df253da2b8d3792690d5831512724a641022
tjs6232 <106604088+tjs6232@users.noreply.github.com>
2022-06-22 10:11:56 -0400

    Create level1test.sh

---
+++ level1test.sh
@@ -0,0 +1,12 @@
+
+
+mkdir "$USER_HASH"
+
+
+echo "***************************************"
+echo "*   Welcome to Level 1 of The PolyLinux Game     *"
+echo "*             Good Luck!              *"
+echo "*    You created this session on:     *"
+echo "*             $currentDate            *"
+echo "***************************************"
+echo "*Use the ls command to find the password*"
