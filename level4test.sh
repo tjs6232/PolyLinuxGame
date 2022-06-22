45f90c72f4f72af7934b02ccfb94fa21aca2b527
tjs6232 <106604088+tjs6232@users.noreply.github.com>
2022-06-22 10:13:05 -0400

    Create level4test.sh

---
+++ level4test.sh
@@ -0,0 +1,21 @@
+
+
+mkdir OldMain
+cd OldMain
+touch Bell
+touch Door
+touch "$USER_HASH"
+touch Presidents
+touch Office
+cd ..
+
+
+
+
+echo "***************************************"
+echo "*   Welcome to Level 4 of The PolyLinux Game     *"
+echo "*             Good Luck!              *"
+echo "*    You created this session on:     *"
+echo "*             $currentDate            *"
+echo "***************************************"
+echo "*Use the cd and ls command to find the password*"
