ce5a920058b8e393810218e37cffe7077d5471a1
tjs6232 <106604088+tjs6232@users.noreply.github.com>
2022-06-22 10:12:15 -0400

    Create level2test.sh

---
+++ level2test.sh
@@ -0,0 +1,22 @@
+
+mkdir BeaverStadium
+
+cd BeaverStadium
+
+mkdir OldMain
+
+cd OldMain
+
+mkdir "$USER_HASH"
+
+cd "$USER_HASH"
+
+
+
+echo "***************************************"
+echo "*   Welcome to Level 2 of The PolyLinux Game     *"
+echo "*             Good Luck!              *"
+echo "*    You created this session on:     *"
+echo "*             $currentDate            *"
+echo "***************************************"
+echo "*Use the pwd command to find the password*"
