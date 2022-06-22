1b1df3e4daf4b1b0d4b4e895631036c9979ffc75
tjs6232 <106604088+tjs6232@users.noreply.github.com>
2022-06-22 10:13:27 -0400

    Create level5test.sh

---
+++ level5test.sh
@@ -0,0 +1,24 @@
+
+
+
+
+mkdir Delta
+cd Delta
+touch inhere.txt
+echo "$USER_HASH" >> inhere.txt
+cd ..
+mkdir Spirit
+mkdir American
+mkdir SouthWest
+mkdir Frontier
+
+
+
+
+echo "***************************************"
+echo "*   Welcome to Level 5 of The PolyLinux Game     *"
+echo "*             Good Luck!              *"
+echo "*    You created this session on:     *"
+echo "*             $currentDate            *"
+echo "***************************************"
+echo "*Use the find command to find the password in a text document called inhere*"
