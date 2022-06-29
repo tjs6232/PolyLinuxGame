db86004f475889df4326b0907f8aa9912c21489c
tjs6232 <106604088+tjs6232@users.noreply.github.com>
2022-06-29 09:39:50 -0400

    Update level3.sh

--- level3.sh
+++ level3.sh
@@ -1,10 +1,20 @@
+#!/bin/bash
+
+#set and confirm inputs
+levelPassword="basic1password"
+levelToBuild="basic1"
+readMeLocation=$levelToBuild"/README.txt"
+
+level_HASH=$(echo -n "$USER_ID$currentDate$newPass$levelPassword" | md5sum | grep -o '^\S\+')
+
+#Create level contents
 mkdir level3
 
 cd level3
 
 touch password.txt
 
- echo "$USER_HASH" >> password.txt
+ echo "$level_HASH" >> password.txt
 
 
 
