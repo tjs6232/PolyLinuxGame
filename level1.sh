9bd859f4fa6593ae45f0e567ef7731418b8d2afa
tjs6232 <106604088+tjs6232@users.noreply.github.com>
2022-06-29 09:37:59 -0400

    Update level1.sh

--- level1.sh
+++ level1.sh
@@ -1,8 +1,18 @@
+#!/bin/bash
+
+#set and confirm inputs
+levelPassword="basic1password"
+levelToBuild="basic1"
+readMeLocation=$levelToBuild"/README.txt"
+
+level_HASH=$(echo -n "$USER_ID$currentDate$newPass$levelPassword" | md5sum | grep -o '^\S\+')
+
+#Create the Level Contents
 mkdir level1
 
 cd level1
 
-mkdir "$USER_HASH"
+mkdir "$level_HASH"
 
 
 echo "***************************************"
