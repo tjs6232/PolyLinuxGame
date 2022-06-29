d8e740c50d131ebfc4e2de6b57482cc521860209
tjs6232 <106604088+tjs6232@users.noreply.github.com>
2022-06-29 09:43:11 -0400

    Update level5.sh

--- level5.sh
+++ level5.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #set and confirm inputs
-levelPassword="basic1password"
-levelToBuild="basic1"
+levelPassword="basic5password"
+levelToBuild="basic15"
 readMeLocation=$levelToBuild"/README.txt"
 
 level_HASH=$(echo -n "$USER_ID$currentDate$newPass$levelPassword" | md5sum | grep -o '^\S\+')
