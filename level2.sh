624c01df0c1ebdf27f74f588a6a588f9559524fe
tjs6232 <106604088+tjs6232@users.noreply.github.com>
2022-07-01 09:37:47 -0400

    Update level2.sh

--- level2.sh
+++ level2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #set and confirm inputs
-levelPassword="basic1password"
-levelToBuild="basic1"
+levelPassword="basic2password"
+levelToBuild="basic2"
 readMeLocation=$levelToBuild"/README.txt"
 
 level_HASH=$(echo -n "$USER_ID$currentDate$newPass$levelPassword" | md5sum | grep -o '^\S\+' | base64 | cut -c 1-8)
@@ -43,7 +43,7 @@ cd ..
 
 
 echo "***************************************"
-echo "*   Welcome to Level 5 of The PolyLinux Game     *"
+echo "*   Welcome to Level 2 of The PolyLinux Game     *"
 echo "*             Good Luck!              *"
 echo "*    You created this session on:     *"
 echo "*             $currentDate            *"
