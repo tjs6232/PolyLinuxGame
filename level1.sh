4b879eb42d0558227948788113cb7aa936fa9e65
tjs6232 <106604088+tjs6232@users.noreply.github.com>
2022-07-06 09:34:34 -0400

    Update level1.sh

--- level1.sh
+++ level1.sh
@@ -16,10 +16,8 @@ cd level1
 mkdir "$level_HASH"
 
 
-echo "***************************************"
-echo "*   Welcome to Level 1 of The PolyLinux Game     *"
-echo "*             Good Luck!              *"
-echo "*    You created this session on:     *"
-echo "*             $currentDate            *"
-echo "***************************************"
-echo "*Use the ls command to find the password*"
+echo "*" > $readMeLocation
+echo "*" >> $readMeLocation
+echo "*" >> $readMeLocation
+echo "Use the ls command to find the password for this level" >> $readMeLocation
+echo "The contents will be the password for this level." >>$readMeLocation
