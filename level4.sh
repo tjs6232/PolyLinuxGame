99fa2e161a77be55d44b5231ad0b8a417a2ba7d4
tjs6232 <106604088+tjs6232@users.noreply.github.com>
2022-07-06 09:58:18 -0400

    Update level4.sh

--- level4.sh
+++ level4.sh
@@ -12,6 +12,19 @@ mkdir level4
 
 cd level4
 
+#create readme.txt
+touch README.txt
+
+echo "***************************************" >> README.txt
+echo "*   Welcome to Level 3 of The PolyLinux Game     *" >> README.txt
+echo "*             Good Luck!              *" >> README.txt
+echo "*    You created this session on:     *" >> README.txt
+echo "*             $currentDate            *" >> README.txt
+echo "***************************************" >> README.txt
+echo "*Use the cd and ls commands to find the password inside the directories*" >> README.txt
+
+#create level contents
+
 mkdir OldMain
 cd OldMain
 touch Bell
@@ -20,6 +33,11 @@ touch "$level_HASH"
 touch Presidents
 touch Office
 cd ..
+
+mkdir BeaverStadium
+mkdir Westgate
+mkdir TheHub
+mkdir Pegula
 
 
 
