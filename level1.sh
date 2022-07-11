a424dcae2c9d9dd77397abd627de20201ee5c935
tjs6232 <106604088+tjs6232@users.noreply.github.com>
2022-07-11 15:21:09 -0400

    Update level1.sh

--- level1.sh
+++ level1.sh
@@ -1,22 +1,21 @@
-#!/bin/bash
+#!/bin/sh
 
 #set and confirm inputs
-levelPassword="basic1password"
-levelToBuild="basic1"
+levelPassword="level1password"
+levelToBuild="level1"
 readMeLocation=$levelToBuild"/README.txt"
 
 level_HASH=$(echo -n "$USER_ID$currentDate$newPass$levelPassword" | md5sum | grep -o '^\S\+' | base64 | cut -c 1-8)
 #echo $level_HASH | base64 | cut -c 1-8 > $levelToBuild/inhere.txt
 
 #Create the Level Contents
-mkdir level1
+mkdir $levelToBuild
 
 cd level1
 
-#create readme.txt
-touch README.txt
 
-echo "***************************************" >> README.txt
+
+echo "***************************************" > README.txt
 echo "*   Welcome to Level 1 of The PolyLinux Game     *" >> README.txt
 echo "*             Good Luck!              *" >> README.txt
 echo "*    You created this session on:     *" >> README.txt
