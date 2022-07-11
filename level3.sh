1f6d7c49c61793dc61c48562cad0b230190bfaf7
tjs6232 <106604088+tjs6232@users.noreply.github.com>
2022-07-11 14:18:37 -0400

    Update level3.sh

--- level3.sh
+++ level3.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
 #set and confirm inputs
-levelPassword="basic3password"
-levelToBuild="basic3"
+levelPassword="level3password"
+levelToBuild="level3"
 readMeLocation=$levelToBuild"/README.txt"
 
 level_HASH=$(echo -n "$USER_ID$currentDate$newPass$levelPassword" | md5sum | grep -o '^\S\+' | base64 | cut -c 1-8)
 
 #Create level contents
-mkdir level3
+mkdir $leveltoBuild
 
-cd level3
+cd $leveltoBuild
 
 #create readme.txt
-touch README.txt
 
-echo "***************************************" >> README.txt
+echo "***************************************" > README.txt
 echo "*   Welcome to Level 3 of The PolyLinux Game     *" >> README.txt
 echo "*             Good Luck!              *" >> README.txt
 echo "*    You created this session on:     *" >> README.txt
@@ -25,9 +24,9 @@ echo "*Use the cat command to find the password inside a text document*" >> READ
 
 
 
-touch password.txt
-touch notinhere.txt
-touch PennState.txt
-touch Westgate.txt
+echo "This is not what you are looking for" > notinhere.txt
+echo "This is not what you are looking for" > PennState.txt
+echo "This is not what you are looking for" > Westgate.txt
 
- echo "$level_HASH" >> password.txt
+
+ echo "$level_HASH" > password.txt
