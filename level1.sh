58bad368d140589a925b90b85412fdff6b05cda5
tjs6232 <106604088+tjs6232@users.noreply.github.com>
2022-07-13 10:11:48 -0400

    Update level1.sh

--- level1.sh
+++ level1.sh
@@ -9,9 +9,9 @@ level_HASH=$(echo -n "$USER_ID$currentDate$newPass$levelPassword" | md5sum | gre
 #echo $level_HASH | base64 | cut -c 1-8 > $levelToBuild/inhere.txt
 
 #Create the Level Contents
-mkdir level1
+mkdir $levelToBuild
 
-cd level1
+cd $levelToBuild
 
 
 
