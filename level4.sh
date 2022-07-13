507abfc6bcb7c6bce8e777525a82e111f9a36265
tjs6232 <106604088+tjs6232@users.noreply.github.com>
2022-07-13 08:39:59 -0400

    Update level4.sh

--- level4.sh
+++ level4.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 #set and confirm inputs
-levelPassword="basic4password"
-levelToBuild="basic4"
+levelPassword="level4password"
+levelToBuild="level4"
 readMeLocation=$levelToBuild"/README.txt"
 
 level_HASH=$(echo -n "$USER_ID$currentDate$newPass$levelPassword" | md5sum | grep -o '^\S\+' | base64 | cut -c 1-8)
@@ -13,9 +13,8 @@ mkdir level4
 cd level4
 
 #create readme.txt
-touch README.txt
 
-echo "***************************************" >> README.txt
+echo "***************************************" > README.txt
 echo "*   Welcome to Level 4 of The PolyLinux Game     *" >> README.txt
 echo "*             Good Luck!              *" >> README.txt
 echo "*    You created this session on:     *" >> README.txt
@@ -38,14 +37,3 @@ mkdir BeaverStadium
 mkdir Westgate
 mkdir TheHub
 mkdir Pegula
-
-
-
-
-echo "***************************************"
-echo "*   Welcome to Level 4 of The PolyLinux Game     *"
-echo "*             Good Luck!              *"
-echo "*    You created this session on:     *"
-echo "*             $currentDate            *"
-echo "***************************************"
-echo "*Use the cd and ls command to find the password*"
